b965bab4c4ba09c72ecae7e1c0dab3efd5770411
Yuriy Epikhin <epih96@mail.ru>
2023-01-17 20:09:53 +0300

    lesson 5 exercise

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,16 @@
-const { src, dest, parallel, watch } = require('gulp');
+const { src, dest } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const pug = require('gulp-pug');
-const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
-browserSyncJob = () => {
-    browserSync.init({
-        server: "build/"
-    });
-
-    watch('src/sass/*.scss', buildSass);
-    watch('src/pages/*.pug', buildPug);
-}
 const buildSass = () => {
     console.log('Компиляция SASS');
 
-    return src('src/sass/*.scss')
-        .pipe(sass())
-        .pipe(dest('build/styles/'))
-        .pipe(browserSync.stream());
+    return src('src/**/*.scss')
+        .pipe(sass({ sourceMap: false }, false))
+        .pipe(cleanCSS())
+        .pipe(concat('app.css'))
+        .pipe(dest('./server/www/styles/'))
 };
 
-const buildPug = () => {
-    console.log('Компиляция Pug');
-
-    return src('src/pages/*.pug')
-        .pipe(pug())
-        .pipe(dest('build/'))
-        .pipe(browserSync.stream());
-};
-
-exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug);
+exports.build = buildSass;
