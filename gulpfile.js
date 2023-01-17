c62b9d0bf2af792d243641b58df8ee3f2165599b
Yuriy Epikhin <epih96@mail.ru>
2023-01-17 18:56:31 +0300

    lesson 4 self-exercise

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,14 @@
-const { watch } = require('gulp');
+const { src, dest, watch } = require('gulp');
 
-const changeAppStylesFile = (done) => {
-    console.log('Ой, файл app.scss изменился');
-
-    done();
-};
-
-const checkFileStructure = (done) => {
-    console.log('Изменилась структура файлов');
-
-    done();
+const copyCSS = () => {
+    return src('project/**/*.css')
+        .pipe(dest('server/dist'));
 };
 
 const watchers = () => {
-    watch('src/sass/app.scss', { events: 'change' }, changeAppStylesFile);
-    watch('src/sass/', { events: ['add', 'unlink'] }, checkFileStructure);
+    watch('project', copyCSS);
 };
 
 exports.watchers = watchers;
+exports.copyCSS = copyCSS;
+exports.default = copyCSS;
