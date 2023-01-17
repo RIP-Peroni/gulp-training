0d88b3778510a93c9179cf711dd791753fd76611
Yuriy Epikhin <epih96@mail.ru>
2023-01-17 12:02:40 +0300

    lesson 3

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,12 @@ const copyScss = () => {
         .pipe(dest('build/styles'));
 };
 
+// const task = () => {
+//     return src('файл с которым работаем')
+//         .pipe(pluginOne()) // Обработка первым плагином
+//         .pipe(pluginTwo()) // Обработка вторым плагином
+//         .pipe(pluginN()) // Обработка ещё каким-нибудь плагином
+//         .pipe(dest('путь, по которому расположим обработанный файл'));
+// };
+
 exports.default = copyScss;
